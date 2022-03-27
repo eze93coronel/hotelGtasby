37ca19d2b9e2989581aa13d4d4995a6122a6331c
roqueCoronel <rokecoronel748@gmail.com>
2022-03-27 16:20:31 -0300

    se agrego plugin de datocms, se consulto la api, se agrego componente con la cnsulta!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import ImagenHotel from '../components/ImagenHotel'
+import ContenidoInicio from '../components/contenidoInicio';
 const IndexPage = () => (
     <Layout>
     <ImagenHotel />
-
+    <ContenidoInicio />
     </Layout>
 
 );
